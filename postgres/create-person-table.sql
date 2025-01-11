acb95e84c47618c0d223f433ff418bc70182df7c
Nikita <kom.niki@mail.ru>
2025-01-11 16:30:15 +0300

    init

---
+++ postgres/create-person-table.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE IF NOT EXISTS person (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	age INT NOT NULL,
+	address TEXT NOT NULL,
+	work TEXT NOT NULL
+);
---
+++ src/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab
+{
+    [ApiController]
+    [Route("/api/v1")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "/persons/")]
+        public IEnumerable<Person> Get()
+        {
+            var repo = new PostgresRepository();
+
+            repo.Add(new Person());
+            return repo.GetAll();
+        }
+
+
+    }
+}
