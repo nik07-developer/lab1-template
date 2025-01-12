82e219b37ce31a5b5deb5b5eb300badda32f27e3
Nikita <kom.niki@mail.ru>
2025-01-12 21:46:13 +0300

    add business logic

--- src/PersonController.cs
+++ src/PersonController.cs
@@ -3,7 +3,7 @@
 namespace Lab
 {
     [ApiController]
-    [Route("/api/v1")]
+    [Route("/api/v1/persons")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
@@ -13,16 +13,51 @@ namespace Lab
             _logger = logger;
         }
 
-        [HttpGet(Name = "/persons/")]
-        public IEnumerable<Person> Get()
+        [HttpGet( "/api/v1/persons/{personId}")]
+        public ActionResult<Person> Get([FromRoute] int personId)
         {
-            return [new Person()];
             var repo = new PostgresRepository();
 
-            repo.Add(new Person());
-            return repo.GetAll();
+            if (repo.TryGet(personId, out var person))
+            {
+                return Ok(person);
+            }
+
+            return NotFound();
         }
 
+        [HttpGet("/api/v1/persons/")]
+        public ActionResult<List<Person>> GetAll()
+        {
+            var repo = new PostgresRepository();
+            return Ok(repo.GetAll());
+        }
 
+        [HttpPost("/api/v1/persons/")]
+        public ActionResult Post([FromBody] Person person)
+        {
+            var repo = new PostgresRepository();
+            var id = repo.Add(person);
+
+            return new CreatedResult($"/api/v1/persons/{id}", null);
+        }
+
+        [HttpDelete("/api/v1/persons/{personId}")]
+        public ActionResult Delete([FromRoute]int personId)
+        {
+            var repo = new PostgresRepository();
+            repo.Delete(personId);
+
+            return Ok();
+        }
+
+        [HttpPatch("/api/v1/persons/{personId}")]
+        public ActionResult Patch([FromRoute] int personId, [FromBody] Person person)
+        {
+            var repo = new PostgresRepository();
+            repo.Replace(personId, person);
+
+            return Ok();
+        }
     }
 }
--- src/PostgresRepository.cs
+++ src/PostgresRepository.cs
@@ -17,65 +17,115 @@ namespace Lab
             _connectionString = $"Server={Host};Username={User};Database={DBname};Port={Port};Password={Password};SSLMode=Prefer";
         }
 
-        public void Add(Person person)
+        public int Add(Person person)
         {
             using var conn = new NpgsqlConnection(_connectionString);
-            try
-            {
-                conn.Open();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return;
-            }
+            conn.Open();
 
-            using var command = new NpgsqlCommand("insert into person (name, age, address, work) values (@n1, @n2, @n3, @n4)", conn);
+            using var command = new NpgsqlCommand("insert into person (name, age, address, work) values (@n1, @n2, @n3, @n4) returning id", conn);
 
             command.Parameters.AddWithValue("n1", $"'{person.Name}'");
             command.Parameters.AddWithValue("n2", person.Age);
             command.Parameters.AddWithValue("n3", $"'{person.Address}'");
             command.Parameters.AddWithValue("n4", $"'{person.Work}'");
-            command.ExecuteNonQuery();
+
+            int id = (int)command.ExecuteScalar();
+
+            conn.Close();
+
+            return id;
         }
 
-        public Person[] GetAll()
+        public List<Person> GetAll()
         {
             var persons = new List<Person>();
+            using var conn = new NpgsqlConnection(_connectionString);
+            conn.Open();
 
-            using (var conn = new NpgsqlConnection(_connectionString))
+            using var command = new NpgsqlCommand("select * from person", conn);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
             {
-                try
+                var person = new Person()
                 {
-                    conn.Open();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    return [];
-                }
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Age = reader.GetInt32(2),
+                    Address = reader.GetString(3),
+                    Work = reader.GetString(4),
+                };
 
-                using var command = new NpgsqlCommand("select * from person", conn);
-                var reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var person = new Person()
-                    {
-                        Id = reader.GetInt32(0),
-                        Name = reader.GetString(1),
-                        Age = reader.GetInt32(2),
-                        Address = reader.GetString(3),
-                        Work = reader.GetString(4),
-                    };
-
-                    persons.Add(person);
-                }
-
-                reader.Close();
+                persons.Add(person);
             }
 
-            return persons.ToArray();
+            reader.Close();
+            conn.Close();
+
+            return persons;
+        }
+
+        public bool TryGet(int id, out Person person)
+        {
+            person = null;
+            bool isFound = false;
+
+            using var conn = new NpgsqlConnection(_connectionString);
+            conn.Open();
+
+            using var command = new NpgsqlCommand($"select * from person where id = {id}", conn);
+            var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+
+                person = new Person()
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Age = reader.GetInt32(2),
+                    Address = reader.GetString(3),
+                    Work = reader.GetString(4),
+                };
+
+                isFound = true;
+            }
+
+
+            reader.Close();
+            return isFound;
+        }
+
+        public void Delete(int id)
+        {
+            using var conn = new NpgsqlConnection(_connectionString);
+            conn.Open();
+
+            using var command = new NpgsqlCommand($"delete from person where id = {id}", conn);
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        public int Replace(int id, Person person)
+        {
+            using var conn = new NpgsqlConnection(_connectionString);
+            conn.Open();
+
+            using var command = new NpgsqlCommand("update person set name = @n1, age = @n2, address = @n3, work = @n4 where id = @n0;", conn);
+
+            command.Parameters.AddWithValue("n0", id);
+            command.Parameters.AddWithValue("n1", $"'{person.Name}'");
+            command.Parameters.AddWithValue("n2", person.Age);
+            command.Parameters.AddWithValue("n3", $"'{person.Address}'");
+            command.Parameters.AddWithValue("n4", $"'{person.Work}'");
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+
+            return id;
         }
     }
 }
